d6dfbfed1f3afa53caed7fddd1d201c66cf54404
Samuel Lampa <samuel.lampa@scilifelab.se>
2023-08-10 17:01:12 +0200

    Parse argument with Clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    command: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("Command: {}", args.command)
 }
