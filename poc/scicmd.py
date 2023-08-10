1d3c4443f1bdd19aa5aa6880a2814ea5f4e0263e
Samuel Lampa <samuel.lampa@scilifelab.se>
2023-08-10 18:33:46 +0200

    Implement skipping on existing output files

--- poc/scicmd.py
+++ poc/scicmd.py
@@ -2,29 +2,39 @@
 import sys
 import os
 import re
-import subprocess
+import subprocess as sub
 
 
 def main():
-    command = sys.argv[0]
+    script = sys.argv[0]
     args = sys.argv[1:]
-
-    for arg in args:
-        if os.path.isfile(arg):
-            print(f"FILE: {arg}")
-        else:
-            print(f"Arg: {arg}")
-
-    print("-" * 80)
-
     args_str = " ".join(args)
+
+    input_paths = []
     input_matches = get_input_matches(args_str)
+    for ph, path in input_matches:
+        input_paths.append(path)
+
+    output_paths = []
     output_matches = get_output_matches(args_str)
+    for ph, path in output_matches:
+        output_paths.append(path)
 
-    matches = input_matches + output_matches
+    command = args_str
+    for path in output_paths:
+        if os.path.exists(path):
+            print(f"Skipping: {command} (Output exists: {path})")
+            return
 
-    for placeholder, path in matches:
-        print(f"Full placeholder: {placeholder}, Path: {path}")
+    print(f"Now executing command: {command} ...")
+    out = sub.run(
+        command, shell=True, stdout=sub.PIPE, stderr=sub.PIPE, text=True, check=True
+    )
+
+    if out.stdout:
+        print(f"OUTPUT: {out.stdout}")
+    if out.stderr:
+        print(f"ERRORS: {out.stderr}")
 
 
 def get_input_matches(text):
@@ -32,10 +42,19 @@ def get_input_matches(text):
     matches = pattern.findall(text)
     return matches
 
+
 def get_output_matches(text):
-    pattern = re.compile("(\{o\:([^\{\}]+)\})")
+    pattern = re.compile("(\{o\:([^\{\}]+)\}|o\:([^\s]+))")
     matches = pattern.findall(text)
-    return matches
+    outputs = []
+    for m in matches:
+        output = tuple(m[0])
+        if m[1]:
+            output = m[0], m[1]
+        elif m[2]:
+            output = m[0], m[2]
+        outputs.append(output)
+    return outputs
 
 
 if __name__ == "__main__":
