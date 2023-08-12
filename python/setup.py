d7db9eedc56f6ebdfbd54738c27a5e68560e05a8
Samuel Lampa <samuel.lampa@scilifelab.se>
2023-08-12 20:32:13 +0200

    Bump version to 0.1.3

--- python/setup.py
+++ python/setup.py
@@ -11,7 +11,7 @@ with open("../README.md") as fobj:
 
 setup(
     name="scicommander",
-    version="0.1.2",
+    version="0.1.3",
     description="A small library for executing shell commands in a reproducible way.",
     long_description=long_description,
     long_description_content_type="text/markdown",
