51772905e41573daa7990401f631943e132eea73
Samuel Lampa <samuel.lampa@scilifelab.se>
2023-11-08 16:32:17 +0100

    Create a simple subshell with a prompt

--- python/scicommander/scishell.py
+++ python/scicommander/scishell.py
@@ -1,1 +1,15 @@
-#TBC
+from cmd import Cmd
+
+
+def main():
+    scishell = SciShell()
+    scishell.prompt = """\033[32mscicmdr > \033[39m"""
+    scishell.cmdloop()
+
+
+class SciShell(Cmd):
+    pass
+
+
+if __name__ == "__main__":
+    main()
